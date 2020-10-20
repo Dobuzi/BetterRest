467bd0cada10e42a3f3c276966e0bb6af16636ab
Dobuzi <kimjw7981@gmail.com>
2020-10-20 14:40:47 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 김종원 on 2020/10/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
